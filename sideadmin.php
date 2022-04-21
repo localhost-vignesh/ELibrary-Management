9828fdc8a7f4061e93bd994007b03392acaf42bd
vignesh <vigneshmca83@gmail.com>
2022-04-21 22:50:24 +0530

    admin nav

---
+++ sideadmin.php
@@ -0,0 +1,20 @@
+<div id="hamburger" class="sidenav">
+		<a href="javascript:void(0)" class="closebtn"  onclick="closeNav()">&times;</a>
+		<a href="ahome.php">Home</a>
+		<a href="View_Student.php">View Student</a>
+		<a href="View_Book.php">View Book</a>
+		<a href="View_com.php">View Comments</a>
+		<a href="View_req.php">View Request</a>
+		<a href="achangepass.php">Change Password</a>
+		<a href="logout.php">Logout</a>
+	</div>
+	<!-- Hamburger -->
+	<span style="font-size: 30px; cursor: pointer;" onclick="openNav()">&#9776;</span>
+		<script type="text/javascript">
+			function openNav(){
+				document.getElementById("hamburger").style.display="block";
+			}
+			function closeNav(){
+				document.getElementById("hamburger").style.display="none";
+			}				
+		</script>
